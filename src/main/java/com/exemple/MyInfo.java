34510af0c50089c8c460fcf6d9382fdd55967475
PabloFLanmarion <pabloflanmarion@gmail.com>
2020-07-28 23:41:37 -0300

    Commit init

---
+++ src/main/java/com/exemple/MyInfo.java
@@ -0,0 +1,35 @@
+package com.exemple;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.info.Info.Builder;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyInfo implements InfoContributor {
+
+	@Value("${application.name}")
+	private String applicationName;
+
+	@Value("${build.version}")
+	private String buildVersion;
+
+	@Value("${build.timestamp}")
+	private String buildTimestamp;
+	
+	@Value("${build.description}")
+	private String buildDescricao;
+
+	private MyInfoObject myInfoObject;
+
+	@Override
+	public void contribute(Builder builder) {
+		myInfoObject = new MyInfoObject();
+		myInfoObject.setApplicationName(applicationName);
+		myInfoObject.setApplicationVersion(buildVersion);
+		myInfoObject.setTimeStamp(buildTimestamp);
+		myInfoObject.setDescricao(buildDescricao);
+		builder.withDetail("infoSystem", myInfoObject);
+	}
+
+}
---
+++ src/main/java/com/exemple/MyInfoObject.java
@@ -0,0 +1,66 @@
+package com.exemple;
+
+public class MyInfoObject {
+
+	private String applicationName;
+	private String applicationVersion;
+	private String timeStamp;
+	private String descricao;
+
+	/**
+	 * @return the applicationName
+	 */
+	public String getApplicationName() {
+		return applicationName;
+	}
+
+	/**
+	 * @param applicationName the applicationName to set
+	 */
+	public void setApplicationName(String applicationName) {
+		this.applicationName = applicationName;
+	}
+
+	/**
+	 * @return the applicationVersion
+	 */
+	public String getApplicationVersion() {
+		return applicationVersion;
+	}
+
+	/**
+	 * @param applicationVersion the applicationVersion to set
+	 */
+	public void setApplicationVersion(String applicationVersion) {
+		this.applicationVersion = applicationVersion;
+	}
+
+	/**
+	 * @return the timeStamp
+	 */
+	public String getTimeStamp() {
+		return timeStamp;
+	}
+
+	/**
+	 * @param timeStamp the timeStamp to set
+	 */
+	public void setTimeStamp(String timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+
+	/**
+	 * @return the descricao
+	 */
+	public String getDescricao() {
+		return descricao;
+	}
+
+	/**
+	 * @param descricao the descricao to set
+	 */
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
